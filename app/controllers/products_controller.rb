e9d85f2bde93bc1210760655f6a8b02835f19e99
Areeb Ijaz <ijazareeb@yahoo.com>
2020-12-02 12:23:40 +1100

    Arch:Product Show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+  def show
+    @product = Product.find(params[:id])
+  end
+  def safety_rating
+    @product = Product.find(params[:id])
+    @safety_rating = Product.Find_by(safety_rating:@product.average_safety_rating_bar)
+  end
+  def efficacy_rating
+
+  end
 end
