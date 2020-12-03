5a3c72b8ca8379521cfd5e674c318398932ddd14
antonio arce <anton.arcegomez@gmail.com>
2020-12-03 13:06:02 +1100

    ARCH:product review creation form running

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,12 @@
 class ProductsController < ApplicationController
+
   def index
     @products = Product.all
   end
+
+   def show
+    @product = Product.find(params[:id])
+    @product_review = ProductReview.new
+  end
+
 end
--- db/migrate/20201130063548_create_product_reviews.rb
+++ db/migrate/20201130063548_create_product_reviews.rb
@@ -1,7 +1,7 @@
 class CreateProductReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :product_reviews do |t|
-      t.string :title, default: "untitled"
+      t.string :title,default: "untitled"
       t.string :content
       t.integer :product_rating
       t.references :user, null: false, foreign_key: true
