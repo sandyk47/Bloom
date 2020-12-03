2ec9f598994eb12b8d15bae604365ab3c06f3367
Alexander Kelso <alexander.kelso@outlook.com>
2020-12-03 13:31:33 +1100

    updated ingredients search bar and added navigational buttons around the site

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,11 @@ class Product < ApplicationRecord
   validates :average_product_rating_stars, presence: true, numericality: true, inclusion: { in: 0..5 }
   validates :average_safety_rating_bar, presence: true, numericality: true, inclusion: { in: 0..5 }
   validates :average_efficacy_rating_bar, presence: true, numericality: true, inclusion: { in: 0..5 }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
