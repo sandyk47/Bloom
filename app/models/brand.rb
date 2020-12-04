71fa24b91271b747b2fa46d7cb02e7483f70de69
Sheila Leveille <sbleveille@gmail.com>
2020-12-04 01:41:40 -0500

    FIX: switched back to scraper seed

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,4 @@
 class Brand < ApplicationRecord
-  has_many :products, dependent: :destroy
+  has_many :products
   validates :name, presence: true, uniqueness: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,7 @@ class Product < ApplicationRecord
   has_many :product_ingredients
   
   has_many :ingredients, through: :product_ingredients
-  has_many :product_reviews, dependent: :destroy
+  has_many :product_reviews
 
   validates :title, presence: true, uniqueness: true
 
