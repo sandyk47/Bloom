707ed28d5494db13810524016eda80cadf6c40be
antonio arce <anton.arcegomez@gmail.com>
2020-12-04 17:08:19 +1100

    ARCH: filter bar almost working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
+
   def index
     if params[:query].present?
       @products = Product.product_and_brand_search(params[:query])
+    elsif params[:tag].present?
+      @products = Product.tagged_with(params[:tag])
     else
       @products = Product.all
     end
@@ -12,4 +15,11 @@ class ProductsController < ApplicationController
     @product_review = ProductReview.new
   end
 
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :brand_id, :description, :average_product_rating_stars, :average_efficacy_rating_bar, :average_safety_rating_bar, tag_list: [])
+  end
 end
+
+
