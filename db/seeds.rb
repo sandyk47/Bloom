a8307499b5664533afe56fbebfaab46e4286901a
Ian Chin <ianchinbv@gmail.com>
2020-12-08 16:27:42 +1100

    added tags to products in seeds, removed some excess tags and corrected tag spelling

--- db/seeds.rb
+++ db/seeds.rb
@@ -104,13 +104,112 @@ ingredient_2.update(description: "Yeast extracts consist of the cell contents of
 ingredient_3.update(description: "Also known as rice bran extract, rice bran oil, Oryza Sativa (Rice Bran) Oil, is the oil extracted from the germ and inner husk of rice. It’s similar to peanut oil, with high percentages of fatty acids—which, by the way, makes it a perfect skin moisturizer! Rice bran oil contains many natural antioxidant phenols, flavonoids, tocopherols, and tocotrienols1. A later study found rice bran oil to contain powerful antioxidants derivatives of ferulic acid, caffeic acid, and vanillic acid2. These antioxidants displayed free strong radical scavenging activity. Vanillic acid and ferulic acid as found in rice bran oil have been shown to have good penetration capabilities for the epidermis and dermis upon topical application. Rice bran oil has also found to be a better source of tocopherol (Vitamin E) and free fatty acids than sunflower oil, safflower oil, and groundnut oil. Rice bran oil contains a higher proportion of short-chain fatty acids than the other oils studied. These are natural emollients and provide good hydration for the skin as well as protecting the skin.")
 
 puts "Creating reviews..."
-product_1 = Product.find(10)
-product_2 = Product.find(35)
-product_3 = Product.find(25)
+product_1 = Product.find(1)
+product_2 = Product.find(2)
+product_3 = Product.find(3)
+product_4 = Product.find(4)
+product_5 = Product.find(5)
+product_6 = Product.find(6)
+product_7 = Product.find(7)
+product_8 = Product.find(8)
+product_9 = Product.find(9)
+product_10 = Product.find(10)
+product_11 = Product.find(11)
+product_12 = Product.find(12)
+product_13 = Product.find(13)
+product_14 = Product.find(14)
+product_15 = Product.find(15)
+product_16 = Product.find(16)
+product_17 = Product.find(17)
+product_18 = Product.find(18)
+product_19 = Product.find(19)
+product_20 = Product.find(20)
+product_21 = Product.find(21)
+product_22 = Product.find(22)
+product_23 = Product.find(23)
+product_24 = Product.find(24)
+product_25 = Product.find(25)
+product_26 = Product.find(26)
+product_27 = Product.find(27)
+product_28 = Product.find(28)
+product_29 = Product.find(29)
+product_30 = Product.find(30)
+product_31 = Product.find(31)
+product_32 = Product.find(32)
+product_33 = Product.find(33)
+product_34 = Product.find(34)
+product_35 = Product.find(35)
+
+skin_concerns = %w(Blemishes Dark\ circles Dehydration Dullness Fine\ lines Loss\ of\ firmness Pigmentation Puffiness Redness Sun\ damage Uneven\ skin\ texture Uneven\ skin\ tone Visible\ pores Wrinkles)
+
+skin_types = ["All", "Extra Oily", "Oily", "Combination", "Dry", "Extra Dry", "Sensitive", "Normal"]
+
+product_2.accreditation_list.add(["Alcohol-free", "Sulphate-free"])
+product_2.save
+product_3.accreditation_list.add("Parabens-free")
+product_3.save
+product_4.accreditation_list.add("Vegan")
+product_4.save
+product_5.accreditation_list.add("Parabens-free")
+product_5.save
+product_6.accreditation_list.add(["Parabens-free", "Alcohol-free"])
+product_6.save
+product_7.accreditation_list.add(["Parabens-free", "Alcohol-free", "Sulphate-free"])
+product_7.save
+product_10.accreditation_list.add(["Parabens-free", "Vegan", "Sulphate-free"])
+product_10.save
+product_11.accreditation_list.add("Parabens-free")
+product_11.save
+product_12.accreditation_list.add("Parabens-free")
+product_12.save
+product_13.accreditation_list.add(["Parabens-free", "Vegan", "Sulphate-free"])
+product_13.save
+product_15.accreditation_list.add(["Parabens-free", "Alcohol-free", "Sulphate-free"])
+product_15.save
+product_16.accreditation_list.add(["Parabens-free", "Vegan", "Sulphate-free"])
+product_16.save
+product_18.accreditation_list.add(["Parabens-free", "Sulphate-free"])
+product_18.save
+product_19.accreditation_list.add(["Parabens-free", "Vegan", "Sulphate-free"])
+product_19.save
+product_20.accreditation_list.add(["Parabens-free", "Oil-free", "Sulphate-free"])
+product_20.save
+product_21.accreditation_list.add(["Parabens-free", "Vegan", "Sulphate-free"])
+product_21.save
+product_22.accreditation_list.add(["Parabens-free", "Vegan", "Sulphate-free"])
+product_22.save
+product_23.accreditation_list.add(["Parabens-free", "Vegan", "Sulphate-free"])
+product_23.save
+product_25.accreditation_list.add(["Parabens-free", "Sulphate-free", "Gluten-free"])
+product_25.save
+product_26.accreditation_list.add("Vegan")
+product_26.save
+product_28.accreditation_list.add("Vegan")
+product_28.save
+product_29.accreditation_list.add(["Parabens-free", "Vegan", "Oil-free"])
+product_29.save
+product_31.accreditation_list.add("Parabens-free")
+product_31.save
+product_32.accreditation_list.add("Parabens-free")
+product_32.save
+product_33.accreditation_list.add("Parabens-free")
+product_33.save
+product_35.accreditation_list.add(["Parabens-free", "Sulphate-free"])
+product_35.save
+
+Product.all.each do |product|
+  product.skin_concern_list.add(skin_concerns.sample)
+  product.save
+end
+
+Product.all.each do |product|
+  product.skin_type_list.add(skin_types.sample)
+  product.save
+end
 
 pro_rev_1 = ProductReview.create!(
 user: user_2,
-product: product_1,
+product: product_10,
 title: "Left dry skin feeling refreshed",
 content: "Amazing product! I normally suffer from dry and easily irritated skin... but with Kylie Skin's face moisturizer I could feel the effects almost immediately, and its kept my skin nice and hydrated throughout the day.",
 product_rating: 5,
@@ -118,7 +217,7 @@ product_rating: 5,
 
 pro_rev_2 = ProductReview.create!(
 user: user_3,
-product: product_2,
+product: product_35,
 title: "COELIACS BEWARE!!!!!!!",
 content: "This skin company is nasty, and not in the good kind of way. I'm a coeliac and this Resurfacing Essence turned my face from a vision of beauty to a rash ridden mess! Clearly the yeast extract comes from barley or something, WHICH THE COMPANY SHOULD DISCLOSE!!!! ",
 product_rating: 1,
@@ -126,7 +225,7 @@ product_rating: 1,
 
 pro_rev_2 = ProductReview.create!(
 user: user_2,
-product: product_2,
+product: product_35,
 title: "A tad expensive, but worth it!",
 content: "Left my face feeling firmed up and ready for summer, highly recommended, wouldn't use it everyday though, tad pricey.",
 product_rating: 4,
@@ -134,7 +233,7 @@ product_rating: 4,
   
 pro_rev_3 = ProductReview.create!(
 user: user_3,
-product: product_3,
+product: product_25,
 title: "YASSSS DIS SOME GOOD SHIT!!!!",
 content: "Goopgenes is da bomb! Left my face feeling amazingly refreshed, and best of all, NO REACTION! Coeliac tested and approved!",
 product_rating: 5,
@@ -142,7 +241,7 @@ product_rating: 5,
   
 pro_rev_4 = ProductReview.create!(
 user: user_2,
-product: product_3,
+product: product_25,
 title: "Not my first choice, but a very close second",
 content: "A beautiful mix that left my skin feeling moisturized, firm and even-toned. As an all in one solution for someone on a budget, you can't go wrong with this! ",
 product_rating: 4,
